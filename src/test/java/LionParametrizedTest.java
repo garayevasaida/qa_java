d5265b3c2f6729ca90cd97ef5045d973dadd8172
Saida Garayeva <garayevasaida@yandex.ru>
2024-02-27 18:01:22 +0300

    ispravila

--- src/test/java/LionParametrizedTest.java
+++ src/test/java/LionParametrizedTest.java
@@ -3,33 +3,50 @@ import com.example.Lion;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class LionParametrizedTest {
-    Feline feline = new Feline();
-    private String sex;
-    private boolean expectedMane;
-
+    Feline feline;
+     String sex;
+    boolean expected;
+    String UnsupportedSex = "Иное";
     public LionParametrizedTest(String sex, boolean expected) {
         this.sex = sex;
-        this.expectedMane = expectedMane;
+        this.expected = expected;
     }
-
     @Parameterized.Parameters
-    public static Object[][] getManeParam() {
+    public static Object[][] getInfo() {
         return new Object[][]{
                 {"Самец", true},
                 {"Самка", false},
         };
     }
+    @Before
+    public void init() {
+        feline = Mockito.mock(Feline.class);
+    }
 
     @Test
-    public void doesHaveNameTest() throws Exception {
-        Lion lion = new Lion(sex,feline);
-        assertEquals(expectedMane, lion.doesHaveMane());
+    public void testDoesHaveMane() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        assertEquals(expected, lion.doesHaveMane());
+    }
+
+
+    @Test(expected = Exception.class)
+    public void otherSex() throws Exception{
+        new Lion (UnsupportedSex,feline);
     }
 }
+
+
+
+
+
+
+
+
